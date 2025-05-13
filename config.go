cb86546d8f7fd1093c515ced24d5131e9533d48d
Ole-Martin Michelsen <om.michelsen@gmail.com>
2025-05-13 07:01:39 +0200

    demo secret

--- config.go
+++ config.go
@@ -1,4 +1,7 @@
 package main
 
-var AzKeyVaultKey = "o5P9W~nxw7bh4JktGMjth2ifLZQrobBYNPWKLaJk"
-var AzKeyVaultKeySecret = "o5P9W~nxw7bh4JktGMjth2ifLZQrobBYNPWKLaJk"
+var awsSecret = "AKIAIOSFODNN7EXAMPLE"
+
+func getSecret() string {
+	return awsSecret
+}
